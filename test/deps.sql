d976e27b0151708244710d92b04117304fbd3b3d
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-11-01 21:04:46 -0500

    Fix extension name for pg_acl

--- test/deps.sql
+++ test/deps.sql
@@ -1,6 +1,6 @@
 -- Note: pgTap is loaded by setup.sql
 
 -- Add any test dependency statements here
-CREATE EXTENSION IF NOT EXISTS acl;
+CREATE EXTENSION IF NOT EXISTS pg_acl;
 CREATE EXTENSION IF NOT EXISTS citext;
 CREATE EXTENSION qgres;
